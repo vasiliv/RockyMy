d8b17cf8777aa3da55bf2c38cc31c631120f2d08
vasiliv <32122468+vasiliv@users.noreply.github.com>
2022-11-14 22:33:57 +0400

    Add project files.

---
+++ RockyMy/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RockyMy.Data;
+using System.Linq;
+
+namespace RockyMy.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }        
+        public IActionResult Index()
+        {
+            return View(_context.Categories.ToList());;
+        }
+    }
+}
---
+++ RockyMy/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace RockyMy.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Display Order must be greater than zero")]
+        public int DisplayOrder { get; set; }
+    }
+}
