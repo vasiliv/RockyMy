9079720eb2a148a5a2bdf66c375d3741ff8ab429
vasiliv <32122468+vasiliv@users.noreply.github.com>
2022-11-17 20:15:22 +0400

    4-3 finished

---
+++ RockyMy/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RockyMy.Data;
+using System.Linq;
+
+namespace RockyMy.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Products.Include(x => x.Category).ToList()) ;
+        }
+
+    }
+}
