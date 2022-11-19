0e7fef645d17bb4e397e36ca9a237517e7fffc4a
vasiliv <32122468+vasiliv@users.noreply.github.com>
2022-11-19 16:48:50 +0400

    4-5 finished

--- RockyMy/Controllers/ProductController.cs
+++ RockyMy/Controllers/ProductController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RockyMy.Data;
 using RockyMy.Models;
+using System.IO;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,9 +13,11 @@ namespace RockyMy.Controllers
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public ProductController(ApplicationDbContext context)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -40,15 +45,43 @@ namespace RockyMy.Controllers
         // instead of Create and Edit
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Upsert(Category category)
+        public IActionResult Upsert(Product model)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Update(category);
+            //if (ModelState.IsValid)
+            //{
+                string uniqueFileName = UploadedFile(model);
+
+                Product product = new Product()
+                {
+                    Name = model.Name,
+                    Description = model.Description,
+                    Price = model.Price,
+                    Image = uniqueFileName,
+                    //gasascorebelia
+                    CategoryId = 1
+                };
+
+                _context.Add(product);
                 _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
+            //}
+            //return View();
+        }
+        private string UploadedFile(Product model)
+        {
+            string uniqueFileName = null;
+
+            if (model.ProfileImage != null)
+            {
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ProfileImage.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.ProfileImage.CopyTo(fileStream);
+                }
             }
-            return View(category);
+            return uniqueFileName;
         }
     }
 }
--- RockyMy/Models/Product.cs
+++ RockyMy/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RockyMy.Models
 {
@@ -11,6 +13,11 @@ namespace RockyMy.Models
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Image { get; set; }
+        
+        [Required(ErrorMessage = "Please choose profile image")]
+        [Display(Name = "Profile Picture")]
+        [NotMapped]
+        public IFormFile ProfileImage { get; set; }
 
         //navigation properties
         public int CategoryId { get; set; }
