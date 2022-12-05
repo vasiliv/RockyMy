85e38c9a0cae2e89a640472d9462fb8e2bc33050
vasiliv <32122468+vasiliv@users.noreply.github.com>
2022-12-05 12:28:11 +0400

    5-2 finished

--- RockyMy/Controllers/HomeController.cs
+++ RockyMy/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using RockyMy.Data;
 using RockyMy.Models;
+using RockyMy.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,15 +15,21 @@ namespace RockyMy.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new HomeVM()
+            {
+                Products= _context.Products.Include(p => p.Category).Include(p => p.ApplicationType)
+            };
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
---
+++ RockyMy/ViewModels/HomeVM.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json.Linq;
+using RockyMy.Models;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace RockyMy.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
